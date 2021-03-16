51440603b17e63b12d119000573611c3c0f6e662
Jason Velarde <51281267+got0values@users.noreply.github.com>
2021-03-16 17:10:41 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <title>FML Computer Sign In</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">
+        FML Computer Sign In
+    </a>
+    <div>
+        <ul class="navbar-nav ml-auto mt-2">
+            <li class="nav-item"><a class="nav-link" href='index.php'>Home</a></li>
+            <li class="nav-item"><a class="nav-link" href='history.php'>History</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+    include_once 'header.php';
+    
+    ini_set('display_errors',1);
+    error_reporting(-1);
+
+?>
+
+<title>Computer Sign In</title>
+
+<script type="text/javascript" src="countdown.js"></script>
+
+<h2 class="text-center mb-5">Sign In</h2>
+
+
+<?php
+if (isset($_POST["bcSubmit"])) {
+    try{
+        // connect to sqlitedb
+        $pdo = new PDO('sqlite:compsignin.db');
+
+        // obtain name
+        $name = $_POST['nameInput'];
+        $computer = $_POST['compInput'];
+        $nowTime = TIME('now', 'localtime');        
+        // insert transaction into db
+        $signInStatement = "INSERT into signin (name, comp, timenow, datenow, timeexp, dateexp) VALUES ('$name', '$computer', TIME('now', 'localtime'), DATE('now', 'localtime'), TIME(TIME('now', 'localtime'), '+60 minutes'), DATE('now', 'localtime'));";
+        $pdo->query($signInStatement);
+
+    }catch (PDOException $e) {
+        echo $e -> getMessage();
+    }
+}
+?>
+
+<div class="container input-group mb-3">
+    <form action="index.php" method="post">
+        <input type="text" name="nameInput" placeholder="Name" id="nameInput" autofocus>
+        <input type="text" name="compInput" placeholder="Computer" id="compInput">
+        <input class="btn btn-outline-secondary" name="bcSubmit" value="Submit" type="submit">
+    </form>
+</div> 
+
+<table class="table table-hover container">
+  <thead>
+    <tr>
+        <th scope="col" class='text-center'>#</th>
+        <th scope="col" class='text-center'>Delete</th>
+        <th scope="col" class='text-center'>Name</th>
+        <th scope="col" class='text-center'>Computer</th>
+        <th scope="col" class='text-center'>Time In</th>
+        <th scope="col" class='text-center'>Time Expired</th>
+        <th scope="col" class='text-center'>Count Down</th>
+        <th scope="col" class='text-center'>Date</th>
+        <th scope="col" class='text-center'>Notes</th>
+    </tr>
+  </thead>
+  <tbody id="tBody">
+    <?php
+        $pdo = new PDO('sqlite:compsignin.db');
+        // $pdo->query($cardQuery);
+
+        // delete query and execution
+        if(isset($_POST["deleteTwo"])) {
+            $transidTwo = $_POST['deleteTrans'];               
+            $deleteSigninQuery = "DELETE FROM signin WHERE id = '$transidTwo'";
+            $pdo->query($deleteSigninQuery);
+        }
+
+        // add notes query and execution
+        if (isset($_POST['notesButton'])) {
+            $notesText = $_POST['notesText'];
+            $transIdThree = $_POST['transIdThree'];
+            $addNotesQuery = "UPDATE signin SET notes = '$notesText' WHERE id = '$transIdThree'";
+            $pdo->exec($addNotesQuery);
+        }
+
+        // list today's patrons query
+        $getCurDateStats = "SELECT * FROM signin WHERE datenow = DATE('now', 'localtime');";
+        $curDateStats = $pdo->query($getCurDateStats);
+        $i = 1;
+        foreach ($curDateStats as $cdsRow) {
+            echo "<tr id='tRow'>";
+            echo    "<td class='text-center'>" . $i++ . "</td>";
+            echo    "<td>"; 
+            echo    "<form action='index.php' method='post'>";
+            echo        "<input type='submit' name='deleteTwo' value='Delete' class='btn btn-danger btn-sm' value='Delete'>";
+            echo        "<input type='hidden' name='deleteTrans' value='$cdsRow[0]'>";
+            echo    "</form>";
+            echo    "</td>";
+            echo    "<td class='text-center'>" . $cdsRow[1] . "</td>";
+            echo    "<td class='text-center'>" . $cdsRow[2] . "</td>";
+            echo    "<td class='text-center'>" . $cdsRow[3] . "</td>";
+            echo    "<td class='text-center'>" . $cdsRow[5] . "</td>";
+            echo    "<td class='text-center' id='tData'>" . "</td>";
+            echo    "<td class='text-center'>" . $cdsRow[4] . "</td>";
+            echo    "<input type='hidden' id='timeexp' value='$cdsRow[5]'>";
+            echo    "<input type='hidden' id='datenow' value='$cdsRow[4]'>";
+            echo    "<td class='text-center'>";
+            echo    "<form action='index.php' method='post'>";
+            echo        "<input type='text' name='notesText' value='$cdsRow[7]'>";
+            echo        "<input type='submit' name='notesButton' value='Save'>";
+            echo        "<input type='hidden' name='transIdThree' value='$cdsRow[0]'>";
+            echo    "</form>";             
+            echo    "</td>";
+            echo "</tr>";
+        }
+    ?>
+  </tbody>
+</table>
+<script type="text/javascript">countDown()</script>
+<script type="text/javascript" src="signin.js">countDown()</script>
+
+
+<?php
+    include_once 'footer.php';
+?>
+
+
+
+
+<!-- https://stackoverflow.com/questions/2663443/need-to-loop-through-a-php-array-in-javascript -->
